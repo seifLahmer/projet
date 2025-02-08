5a06b6b6929e7a4a771aa87c5188ae0dddd05116
seifLahmer <131596701+seifLahmer@users.noreply.github.com>
2025-02-08 09:39:36 +0100

    Ajout de la génération de PDF avec tableau

---
+++ src/main/java/Controllers/ActivitesReservees.java
@@ -0,0 +1,17 @@
+package Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.FlowPane;
+
+public class ActivitesReservees {
+
+    @FXML
+    private FlowPane activitesReserveesContainer;
+
+    @FXML
+    void retourActivites(ActionEvent event) {
+
+    }
+
+}
