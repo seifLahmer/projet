083c9044d433a7d342019c42bcafcc24ed23f627
Mafiosa47 <baccouche1907@gmail.com>
2025-02-11 13:05:09 +0100

    ..

--- src/main/java/Services/IService.java
+++ src/main/java/Services/IService.java
@@ -1,14 +1,19 @@
 package Services;
 
+import Entite.Payment;
+import Entite.Product;
+
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
 public interface IService<T> {
     void ajouter(T t) throws SQLException;
     void supprimer (T t ) throws SQLException;
-    void update (T t , Map<String, Object> data) throws SQLException ;
+    void update (T t ) throws SQLException ;
+
+
     List<T> getAll () throws SQLException ;
     T getById (int id ) throws SQLException ;
-
 }
